81f74451d8ba6cea11a8a5e0230e8412284a1a09
Leon Tassinari <43296064+Tassy210@users.noreply.github.com>
2023-07-02 22:58:24 -0300

    Integração Flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from pathlib import Path
+import gpt
+
+app = Flask(__name__)
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("./assets")
+
+def send(assunto, materia):
+    if assunto != '' or materia != '':
+        messageLabel = gpt.chat(materia, assunto)
+        respostas = gpt.respostas(messageLabel)
+        return respostas
+    else:
+        return 'Por favor, insira algo na caixa de texto'
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        assunto = request.form.get('assunto')
+        materia = request.form.get('materia')
+        respostas = send(assunto, materia)
+        return render_template('index.html', respostas=respostas, assunto=assunto, materia=materia)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,3 @@
+url = "https://api.openai.com/v1/chat/completions"
+token= "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"
+model = "gpt-3.5-turbo"
